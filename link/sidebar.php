7a5c4de8abfb9a9570f2a8af80403b4b448820dd
牛久幹太 <kantaiphoneau@gmail.com>
2019-12-06 19:57:33 +0900

    編集

--- link/sidebar.php
+++
@@ -1,6 +0,0 @@
-<section id="sidebar">
-    <a href="main.php">投稿する</a>
-    <a href="passproduct.php">パスワード変更</a>
-    <a href="product.php">プロフィール編集</a>
-    <a href="withdraw.php">退会</a>
-</section>
